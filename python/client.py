640c0addf612675d38045c1270934b43e5b8a982
mts0629 <soratobi93@gmail.com>
2025-04-27 10:27:36 +0900

    Echo back  with specified address and port

--- python/client.py
+++ python/client.py
@@ -1,21 +1,71 @@
+import argparse
 import socket
 
 
-HOST = "127.0.0.1"
-PORT = 8080
+BUF_SIZE = 1024
+
+
+# Command line arguments:
+# - server address
+# - port number
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Example echo back client"
+    )
+    parser.add_argument(
+        "srv_addr", type=str, help="Server address"
+    )
+    parser.add_argument(
+        "port_num", type=int, help="Port number"
+    )
+
+    return parser.parse_args()
 
 
 def main() -> None:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        print(f"Connect to a server {HOST}:{PORT}...")
+    args = _parse_args()
 
-        sock.connect((HOST, PORT))
+    # Open a socket
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except OSError:
+        print("socket() failed")
+        return
 
-        print("Connected")
+    # Connect to a server
+    try:
+        print(f"Connect to a server {args.srv_addr}:{args.port_num}...")
+        sock.connect((args.srv_addr, args.port_num))
+    except OSError as msg:
+        print("bind() failed")
+        sock.close()
+        return
 
-        sock.sendall(b"Hello")
+    print("Connected")
 
-        print("Connection closed")
+    while True:
+        # Input string
+        send_str = input("Sending > ")
+        if len(send_str) > BUF_SIZE:
+            print(f"String is too long: length must be <= {BUF_SIZE}")
+            continue
+
+        # Send the string to the server
+        sent_size = sock.send(send_str.encode())
+        if len(send_str) != sent_size:
+            print("send() failed")
+            break
+
+        # Receive a echo-backed string from the server
+        data = sock.recv(BUF_SIZE)
+        if not data:
+            print("recv() failed")
+            break
+
+        print(f"Received: {data.decode()}")
+
+    sock.close()
+    print("Connection closed")
 
 
 if __name__ == "__main__":
--- python/server.py
+++ python/server.py
@@ -1,29 +1,84 @@
+import argparse
 import socket
 
 
-HOST = "127.0.0.1"
-PORT = 8080
 BUF_SIZE = 1024
 
 
+# Command line arguments:
+# - server address
+# - port number
+def _parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(
+        description="Example echo back server"
+    )
+    parser.add_argument(
+        "srv_addr", type=str, help="Server address"
+    )
+    parser.add_argument(
+        "port_num", type=int, help="Port number"
+    )
+
+    return parser.parse_args()
+
+
 def main() -> None:
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-        sock.bind((HOST, PORT))
-        print(f"Open a socket: {HOST}:{PORT}")
+    args = _parse_args()
 
+    # Open a socket
+    try:
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    except OSError:
+        print("socket() failed")
+        return
+
+    # Bind
+    try:
+        sock.bind((args.srv_addr, args.port_num))
+    except OSError:
+        print("bind() failed")
+        sock.close()
+        return
+
+    print(f"Open a socket: {args.srv_addr}:{args.port_num}")
+
+    # Listen
+    try:
         sock.listen()
+    except OSError as msg:
+        print("listen() failed")
+        sock.close()
+        return
 
-        print("Wait connection from a client...")
+    print("Wait connection from a client...")
 
-        conn, _ = sock.accept()
-        with conn:
-            print("Connected")
+    # Accept
+    conn, _ = sock.accept()
+    if not conn:
+        print("accept() failed")
+        sock.close()
+        return
 
+    with conn:
+        print("Connected")
+
+        while True:
+            # Receive data from a client
             data = conn.recv(BUF_SIZE)
+            if not data:
+                print("recv() failed")
+                break
 
             print(f"Received: {data.decode()}")
 
-        print("Connection closed")
+            # Echo back to the client
+            sent_size = conn.send(data)
+            if len(data) != sent_size:
+                print("send() failed")
+                break
+
+    sock.close()
+    print("Connection closed")
 
 
 if __name__ == "__main__":
