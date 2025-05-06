c0298ece98abb639eb55dd457a5914ae759d6d4e
mts0629 <soratobi93@gmail.com>
2025-05-06 15:42:46 +0900

    Echo back

--- go/client.go
+++ go/client.go
@@ -1,26 +1,57 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"net"
+	"os"
+	"strings"
 )
 
 func main() {
 	port := "127.0.0.1:8080"
 
+	// Socket and connect
+	fmt.Println("Connect to a server", port)
 	conn, err := net.Dial("tcp", port)
 	if err != nil {
-		panic(err)
+		fmt.Println("connect() failed")
+		return
 	}
+
+	fmt.Println("Connected")
 
 	defer conn.Close()
 
-	buf := make([]byte, 1024)
+	buf_size := 1024
 
-	count, err = conn.Read(buf)
-	if err != nil {
-		panic(err)
+	for {
+		// Input string
+		fmt.Print("Sending > ")
+		line, err := bufio.NewReader(os.Stdin).ReadString('\n')
+		if err != nil {
+			fmt.Println("Reading stdin failed, please retry:")
+			continue
+		}
+		line = strings.TrimSuffix(line, "\n") // Trim a line feed
+
+		// Send the string to the server
+		_, err = conn.Write([]byte(line))
+		if err != nil {
+			fmt.Println("write() failed")
+			break
+		}
+
+		// Receive a echo-backed string from the server
+		buf := make([]byte, buf_size)
+		count, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("read() failed")
+			break
+		}
+
+		fmt.Println("Received:", string(buf[:count]))
 	}
 
-	fmt.Println(string(buf[:count]))
+	fmt.Println("Connection closed")
 }
--- go/server.go
+++ go/server.go
@@ -1,28 +1,55 @@
 package main
 
 import (
+	"fmt"
 	"net"
 )
 
 func main() {
 	port := "127.0.0.1:8080"
 
+	// Socket, bind and listen
 	listener, err := net.Listen("tcp", port)
 	if err != nil {
-		panic(err)
+		fmt.Println("listen() failed")
+		return
 	}
+
+	fmt.Println("Open a socket:", port)
+	fmt.Println("Wait connection from a client...")
+
+	// Accept
+	conn, err := listener.Accept()
+	if err != nil {
+		fmt.Println("accept() failed")
+		return
+	}
+
+	fmt.Println("Connected")
+
+	defer conn.Close()
+
+	buf_size := 1024
 
 	for {
-		conn, err := listener.Accept()
+		buf := make([]byte, buf_size)
 
+		// Receive data from a client
+		count, err := conn.Read(buf)
 		if err != nil {
-			continue
-		} else {
-			go func() {
-				defer conn.Close()
+			fmt.Println("read() failed")
+			break
+		}
 
-				conn.Write([]byte("hello"))
-			}()
+		fmt.Println("Received:", string(buf[:count]))
+
+		// Echo back to the client
+		_, err = conn.Write(buf)
+		if err != nil {
+			fmt.Println("write() failed")
+			break
 		}
 	}
+
+	fmt.Println("Connection closed")
 }
